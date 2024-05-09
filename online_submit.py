bd2da98aefb5109c3eac3c49c97c16f3ed21d3ad
FlowerGrid <FlowerGridCoding@gmail.com>
2024-05-08 19:36:14 -0500

    fixed waiting issue, should wait for login to be complete and submission page to load

--- online_submit.py
+++ online_submit.py
@@ -2,6 +2,9 @@ import os
 import pyperclip
 import re
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from tkinter import *
 import tkinter.filedialog as filedialog
@@ -34,89 +37,64 @@ def create_submit_window(final_file, lyric_file):
     link_entry = Entry(main_frame, width=30, borderwidth=2)
     link_entry.grid(row=0, column=1, columnspan=2, sticky='w')
 
-    '''
-    # Username & Password input
-    username_label = Label(main_frame, text='Username:', padx=5, pady=5)
-    username_label.grid(row=1, column=0)
-    username_entry = Entry(main_frame, width=45, borderwidth=2)
-    username_entry.grid(row=1, column=1, columnspan=2)
-
-    password_label = Label(main_frame, text='Password:', padx=5, pady=5)
-    password_label.grid(row=2, column=0)
-    password_entry = Entry(main_frame, width=45, borderwidth=2, show='💩')
-    password_entry.grid(row=2, column=1, columnspan=2)
-    '''
-
     lang_list = ['en', 'sp', 'fr', 'de']
     lang_label = Label(main_frame, text='Lyrics Language', padx=5, pady=5)
     lang_label.grid(row=1, column=0, sticky='e')
 
-    lang_combo_box = ttk.Combobox(main_frame, values=lang_list, state='readonly')
-    lang_combo_box.grid(row=1, column=1)
+    lang_combo_box = ttk.Combobox(main_frame, values=lang_list, width=4, state='readonly')
+    lang_combo_box.current(0)
+    lang_combo_box.grid(row=1, column=1, sticky='w')
+
+    lyric_checkbox_bool = IntVar()
+    lyric_checkbox = Checkbutton(main_frame, text="Copy Lyrics", variable=lyric_checkbox_bool)
+    lyric_checkbox.grid(row=1, column =2, sticky='w')
 
     submit_button = Button(main_frame, text='Submit', command=lambda: online_submit(
-        final_file, lyric_file, link_entry, lang_combo_box, submit_window
+        final_file, lyric_file, link_entry.get(), lang_combo_box.get(), lyric_checkbox_bool.get(), submit_window
         ))
+    
     submit_button.grid(row=2, column=1)
 
     submit_window.mainloop()
 
 
-def online_submit(final_file, lyric_file, link_entry, lang_combo_box, submit_window):
-    # Pull user input from gui
-    song_link = link_entry.get()
-    #username = username_entry.get()
-    #password = password_entry.get()
-    lang = lang_combo_box.get()
-
+def online_submit(final_file, lyric_file, song_link, lang, lyric_checkbox_bool, submit_window):
+    print('===got here===')
     # Initialize Selenium WebDriver (assuming Chrome)
     driver = webdriver.Chrome()
 
     # Load the submission page
     driver.get(song_link)
 
-    '''
-    # Deal with login credentials
-    # Need help from Senyo with this
-    username_field = driver.find_element('id', "username")
-    username_field.send_keys(username)
-
-    password_field = driver.find_element('id', 'password')
-    password_field.send_keys(password)
-    '''
+    username = driver.find_element('id', 'username')
+    username.send_keys('did it work?')
 
     # Wait for user to login
-    revealed = driver.find_element('class_name', 'chartbuilder-input')
+    try:
+        wait = WebDriverWait(driver, 30)
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "chartbuilder-input")))
 
-    wait = WebDriverWait(driver, timeout=15)
-    wait.until(lambda d : revealed.is_displayed())
+        # Read and parse the text file to extract sections
+        with open(final_file, "r") as file:
+            sections = file.read().split("\n\n")
 
-    # Read and parse the text file to extract sections
-    with open(final_file, "r") as file:
-        sections = file.read().split("\n\n")
+        # Fill out each input field with the corresponding section
+        for i, section in enumerate(sections[2:]):
+            lines = section.split('\n')
+            section_id = re.search(r'#(\d+)', lines[0]).group(1)
 
-    # Fill out each input field with the corresponding section
-    for i, section in enumerate(sections[2:]):
-        lines = section.split('\n')
-        section_id = re.search(r'#(\d+)', lines[0]).group(1)
+            section = '\n'.join(line for line in lines[1:])
+            input_field = driver.find_element("id", f"coda_{section_id}")
+            pyperclip.copy(section)
+            input_field.send_keys(pyperclip.paste())
 
-        section = ''.join(line for line in lines[1:])
-        input_field = driver.find_element("id", f"coda_{section_id}")
-        pyperclip.copy(section)
-        input_field.send_keys(pyperclip.paste())
 
-    # Read and parse lyric file
-    with open(lyric_file, 'r') as l_file:
-        l_sections = l_file.read().split('\n\n')
+        if lyric_checkbox_bool == 1:
+            lyrics(lyric_file, driver, lang)
 
-    for lyric in l_sections:
-        lines = lyric.split('\n')
-        lyric_id = re.search(r'#(\d+)', lines[0]).group(1)
-
-        lyric = ''.join(line for line in lines[1:])
-        l_input_field = driver.find_element('id', f'{lang}_chordPro_{lyric_id}') 
-        pyperclip.copy(lyric)
-        l_input_field.send_keys(pyperclip.paste())
+    except TimeoutException:
+        print('Timeout')
+        driver.quit()
 
 
     # Submit the form
@@ -129,6 +107,21 @@ def online_submit(final_file, lyric_file, link_entry, lang_combo_box, submit_win
     submit_window.destroy()
 
 
+def lyrics(lyric_file, driver, lang):
+    # Read and parse lyric file
+    with open(lyric_file, 'r') as l_file:
+        l_sections = l_file.read().split('\n\n')
+
+    for lyric in l_sections:
+        lines = lyric.split('\n')
+        lyric_id = re.search(r'#(\d+)', lines[0]).group(1)
+
+        lyric = '\n'.join(line for line in lines[1:])
+        l_input_field = driver.find_element('id', f'{lang}_chordPro_{lyric_id}') 
+        pyperclip.copy(lyric)
+        l_input_field.send_keys(pyperclip.paste())
+
+
 if __name__ == '__main__':
     project_path = filedialog.askdirectory(
     title='Select project location',
@@ -137,6 +130,5 @@ if __name__ == '__main__':
     title = os.path.basename(project_path)
     final_file = os.path.join(project_path, 'Final', f'converted_{title}.txt')
     lyric_file = os.path.join(project_path, 'Final', f'{title} LYRICS.txt')
-    print(final_file)
-    print(lyric_file)
+
     create_submit_window(final_file, lyric_file)
