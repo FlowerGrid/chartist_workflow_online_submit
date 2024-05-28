617e31d84414bb717fce4fe86723eaf3629b1dd8
FlowerGrid <FlowerGridCoding@gmail.com>
2024-05-27 23:47:06 -0500

    uses the name attribute for lyrics, renders lang unnecessary

--- online_submit.py
+++ online_submit.py
@@ -41,6 +41,7 @@ def create_submit_window(tk_root, final_file, lyric_file):
     link_entry = Entry(main_frame, width=30, borderwidth=2)
     link_entry.grid(row=0, column=1, columnspan=2, sticky='w')
 
+    '''
     lang_list = ['en', 'sp', 'fr', 'de']
     lang_label = Label(main_frame, text='Lyrics Language', padx=5, pady=5)
     lang_label.grid(row=1, column=0, sticky='e')
@@ -48,13 +49,14 @@ def create_submit_window(tk_root, final_file, lyric_file):
     lang_combo_box = ttk.Combobox(main_frame, values=lang_list, width=4, state='readonly')
     lang_combo_box.current(0)
     lang_combo_box.grid(row=1, column=1, sticky='w')
-
+    '''
+    
     lyric_checkbox_bool = IntVar()
     lyric_checkbox = Checkbutton(main_frame, text="Copy Lyrics", variable=lyric_checkbox_bool)
     lyric_checkbox.grid(row=1, column =2, sticky='w')
 
     submit_button = Button(main_frame, text='Submit', command=lambda: online_submit(
-        driver, final_file, lyric_file, link_entry.get(), lang_combo_box.get(), lyric_checkbox_bool.get(), submit_window
+        driver, final_file, lyric_file, link_entry.get(), lyric_checkbox_bool.get(), submit_window
         ))
     
     submit_button.grid(row=2, column=1)
@@ -62,10 +64,7 @@ def create_submit_window(tk_root, final_file, lyric_file):
     submit_window.mainloop()
     driver.quit()
 
-def online_submit(driver, final_file, lyric_file, song_link, lang, lyric_checkbox_bool, submit_window):
-    # Initialize Selenium WebDriver (assuming Chrome)
-    #driver = webdriver.Chrome()
-
+def online_submit(driver, final_file, lyric_file, song_link, lyric_checkbox_bool, submit_window):
     # Load the submission page
     driver.get(song_link)
 
@@ -75,7 +74,7 @@ def online_submit(driver, final_file, lyric_file, song_link, lang, lyric_checkbo
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "chartbuilder-input")))
 
         if lyric_checkbox_bool == 1:
-            lyrics(lyric_file, driver, lang)        
+            lyrics(lyric_file, driver) # took out lang variable, shouldn't need it anymore
 
         # Read and parse the text file to extract sections
         with open(final_file, "r") as file:
@@ -99,13 +98,8 @@ def online_submit(driver, final_file, lyric_file, song_link, lang, lyric_checkbo
     # Submit the form
     driver.find_element("id", "btnSave").click()
 
-    # Close the browser
-    #driver.quit()
 
-    #submit_window.destroy()
-
-
-def lyrics(lyric_file, driver, lang):
+def lyrics(lyric_file, driver): # took out lang variable, shouldn't need it anymore
     # Read and parse lyric file
     with open(lyric_file, 'r') as l_file:
         l_sections = l_file.read().split('\n\n')
@@ -115,7 +109,7 @@ def lyrics(lyric_file, driver, lang):
         lyric_id = re.search(r'#(\d+)', lines[0]).group(1)
 
         lyric = '\n'.join(line for line in lines[1:])
-        l_input_field = driver.find_element('id', f'{lang}_chordPro_{lyric_id}') 
+        l_input_field = driver.find_element_by_xpath(f"//textarea[@name='chordPro_{lyric_id}']")
         pyperclip.copy(lyric)
         l_input_field.send_keys(pyperclip.paste())
 
